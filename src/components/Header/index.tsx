a8289ad295e24abdb21e76cb5b0582d6e714314e
ThauanyAA <thauanyaraujo5@gmail.com>
2025-06-10 17:35:18 -0300

    Transaction modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,24 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from '@radix-ui/react-dialog';
-
+import { NewTransactionModal } from "../Summary/NewTransactionModal";
 import logoImg from "../../assets/logo.svg";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={logoImg} alt="" />
+        <span>
+          <img src={logoImg} alt="" />
+          DT Money
+        </span>
+        
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
 
-      <Dialog.Root>
-        <Dialog.Trigger asChild>
-          <NewTransactionButton>Nova transação</NewTransactionButton>
-        </Dialog.Trigger>
-
-        <Dialog.Portal>
-          <Dialog.Overlay />
-
-          <Dialog.Content>
-            <Dialog.Title>Nova Transação</Dialog.Title>
-
-            <Dialog.Close />
-          </Dialog.Content>
-        </Dialog.Portal>
-      </Dialog.Root>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
